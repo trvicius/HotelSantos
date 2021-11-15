f146d570e0286df041a88cb0448a9b3d847e294e
Vinicius Galvao de Franca <vinicius.gfranca98@gmail.com>
2021-11-15 19:34:33 -0300

    Commit Contato

--- src/Contato.js
+++ src/Contato.js
@@ -4,28 +4,31 @@ import './Contato.css'
 function Contato (){
 
     return(
-       <div className="global">
-           <div className="end">
+       <div class="container">
+           <header>
                <h2>Hotel Santos</h2>
                 <p>Endereço: Av. Pinheiro Machado, 27</p>
                 <p>CEP 11015-710</p>
-           </div>
-            <div className="tel">
+                <br/>
+            </header>
+            <main>
                 <h2>Telefones de contato</h2>
                 <p> +55 (13) 3256-2626</p>
                 <p>Whatsapp: (13) 97408-8545</p>
-            </div>
-            <div className="atend">
+                <br/>
+            </main>
+            <main>
                 <h2>Atendimento ao cliente</h2>
                 <p>Segunda a Sábado</p>
                 <p>07:00 às 23:00 horas</p>
-            </div>
-            <div className="redes">
+                <br/>
+            </main>
+            <main>
                 <h2>Redes Sociais e E-mail para contato</h2>
-                    <p>E-mail: hshoteis@hotelsantos.com.br</p>
-                    <p>Instagran: @HShoteis</p>
-                    <p>Facebook: HotelSantos013</p>
-            </div>
+                <p>E-mail: hshoteis@hotelsantos.com.br</p>
+                <p>Instagran: @HShoteis</p>
+                <p>Facebook: HotelSantos013</p>
+            </main>
         </div>
     )
 };
